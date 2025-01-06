7b6e0dccbc5d1d104905ecc7958dd379bdee648d
Emilio <emifont01@gmail.com>
2025-01-06 09:50:57 -0500

    feat: parse return statements

--- ast/ast.go
+++ ast/ast.go
@@ -56,3 +56,12 @@ func (p *Program) TokenLiteral() string {
 		return ""
 	}
 }
+
+type ReturnStatement struct {
+	Token       token.Token
+	ReturnValue Expression
+}
+
+func (rs *ReturnStatement) statementNode() {}
+
+func (rs *ReturnStatement) TokenLiteral() string { return rs.Token.Literal }
