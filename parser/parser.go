e3d9e6eafc1fe9d403a7f0004de5b0cdf2911bea
Emilio <emifont01@gmail.com>
2025-02-15 18:52:04 -0500

    chore: remove traces

--- parser/parser.go
+++ parser/parser.go
@@ -210,7 +210,6 @@ func (p *Parser) parseExpressionStatement() *ast.ExpressionStatement {
 }
 
 func (p *Parser) parseExpression(precedence int) ast.Expression {
-	defer untrace(trace("parseExpression"))
 	prefix := p.prefixParseFns[p.curToken.Type]
 
 	if prefix == nil {
